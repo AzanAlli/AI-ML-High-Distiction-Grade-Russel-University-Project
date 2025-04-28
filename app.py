b075e0fbb8e81fbff7f39cddfd6cafa41aab29d2
Azan <sa1021@student.le.ac.uk>
2025-04-28 01:08:48 +0100

    added some comments

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask_cors import CORS
 from flask import Flask, request, jsonify, send_from_directory
 import subprocess
 
+# Initialise Flask app
 app = Flask(__name__, static_folder="frontend_face_info/static", static_url_path="/static")
 CORS(app)
 
@@ -9,8 +10,10 @@ CORS(app)
 def serve_index():
     return send_from_directory("frontend_face_info", "index.html")
 
+# API endpoint to trigger the prediction based on input type
 @app.route('/predict', methods=['POST'])
 def predict():
+    # Parse JSON request data to web server
     input_type = request.json.get('input_type')
     image_path = request.json.get('image_path', '')
 
